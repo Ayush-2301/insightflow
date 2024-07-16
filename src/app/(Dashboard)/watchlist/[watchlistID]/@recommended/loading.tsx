b3e13f1c110f9f59d98dca369543412a5b1c62ed
Ayush-2301 <ayushchauhan233@gmail.com>
2024-07-16 15:13:58 +0530

    updated the company form and watchlist item

--- src/app/(Dashboard)/watchlist/[watchlistID]/@recommended/loading.tsx
+++
@@ -1,5 +0,0 @@
-import { WatchlistsSkeleton } from "../../components/Watchlists";
-
-export default function loading() {
-  return <WatchlistsSkeleton />;
-}
--- src/app/(Dashboard)/watchlist/components/WatchlistServer.tsx
+++
@@ -1,9 +0,0 @@
-import { getAllWatchlist } from "@/lib/actions/watchlist";
-import Watchlists from "./Watchlists";
-
-const WatchlistServer = async () => {
-  const watchlists = await getAllWatchlist();
-  return <Watchlists watchlists={watchlists} />;
-};
-
-export default WatchlistServer;
