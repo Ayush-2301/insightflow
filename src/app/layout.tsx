827ad8eb613f63a23a5ec232cbf22a4c9afe54bf
Ayush-2301 <ayushchauhan233@gmail.com>
2024-08-12 17:40:30 +0530

    Added speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { cn } from "@/lib/utils";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={cn(inter.className, "text-primary")}>
         {children}
+        <SpeedInsights />
         <Toaster />
       </body>
     </html>
